09ab20ee22432c09a5b3e02025a5c8fb7db6d4a4
petersamson-kingori <peters.kingori@moringaschool.com>
2023-06-29 18:12:43 +0300

    Profile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { useAuth } from "./auth";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const UserProfile = () => {
-  const { user } = useAuth();
+  const { user, removeUser } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const redirectPath = location.state?.path || "/login";
 
-  console.log("User:", user); // Add this line to log the user object
+  console.log("User:", user);
+
+  function handleLogoutClick() {
+    fetch("https://crave-masters-front-end.onrender.com/api/v1/logout", { method: "DELETE" })
+      .then((res) => {
+        if (res.ok) {
+          removeUser();
+          navigate(redirectPath, { replace: true });
+        }
+      });
+  }
 
   return (
     <div>
       {user ? (
-        <h2>Welcome, {user.username}!</h2>
+        <div>
+          <h2>Welcome, {user.username}!</h2>
+          <button onClick={handleLogoutClick}>Logout</button>
+        </div>
       ) : (
         <h2>Welcome, Guest!</h2>
       )}
