1780001d5096e5272eb4fb776b32f13fbb52f793
petersamson-kingori <peters.kingori@moringaschool.com>
2023-07-01 21:30:05 +0300

    caterer auth

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -14,6 +14,7 @@ import Reviews from "../components/UI/reviews/reviews";
 import { AuthProvider } from "../pages/auth";
 import RequireAuth from "../pages/RequireAuth";
 import UserProfile from "../pages/UserProfile";
+import UserProfile_Caterer from "../pages/UserProfile_Caterer";
 // import ProtectedRoutes from "./proutes";
 
 const Routers = () => {
