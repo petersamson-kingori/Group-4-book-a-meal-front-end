e642b8ea11580c4fdabeb5194e99eff44152d7a7
petersamson-kingori <peters.kingori@moringaschool.com>
2023-07-05 17:48:18 +0300

    layout

--- src/pages/MenuOptionForm.jsx
+++ src/pages/MenuOptionForm.jsx
@@ -42,7 +42,7 @@ const MenuOptionForm = ({ menuId }) => {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column"}}>
+    <div>
       <h2>Menu</h2>
       <form className="form mb-5" onSubmit={handleSubmit}>
         <h5 className="md:text-2xl text-xl my-4 font-semibold text-gray-800">
