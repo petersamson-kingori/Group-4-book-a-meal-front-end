d9c5d71bcbbf1ec9ed81c17ca67b576ae6a75616
petersamson-kingori <peters.kingori@moringaschool.com>
2023-07-05 16:04:22 +0300

    layout

--- src/pages/MenuOptionForm.jsx
+++ src/pages/MenuOptionForm.jsx
@@ -44,7 +44,7 @@ const MenuOptionForm = ({ menuId }) => {
   return (
     <div>
       <h3>Add Menu Option</h3>
-      <form onSubmit={handleSubmit} style={{ backgroundColor: "#20c997", padding: "20px", borderRadius: "5px" }}>
+      <form onSubmit={handleSubmit} style={{ backgroundColor: "#d6f1e4", padding: "20px", borderRadius: "5px" }}>
   <div>
     <label htmlFor="name">Name:</label>
     <input
