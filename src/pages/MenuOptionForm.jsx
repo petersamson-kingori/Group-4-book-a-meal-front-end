a2a673ab3aaa851415dd3d341fcc9296a3e170c2
petersamson-kingori <peters.kingori@moringaschool.com>
2023-07-05 18:41:36 +0300

    layout

--- src/pages/MenuOptionForm.jsx
+++ src/pages/MenuOptionForm.jsx
@@ -43,7 +43,7 @@ const MenuOptionForm = ({ menuId }) => {
 
   return (
     <div>
-       <h2>Menu</h2>
+       <h2 style={{ paddingLeft: '50px' }}>Menu</h2>
       <form className="form mb-5" onSubmit={handleSubmit}>
   <h5 className="md:text-2xl text-xl my-4 font-semibold text-gray-800">
     Add Option
