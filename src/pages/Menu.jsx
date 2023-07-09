a9bd8e93221a96aab3303738edc51b6bd5139473
petersamson-kingori <peters.kingori@moringaschool.com>
2023-07-09 15:21:16 +0300

    redux

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,8 @@
-// ExampleComponent.js
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setMenuOptions } from '../store/menuSlice';
 
-const Menu = () => {
+const Menu = ({ user }) => {
   const dispatch = useDispatch();
   const menuOptions = useSelector((state) => state.menu);
 
@@ -12,36 +10,49 @@ const Menu = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          'https://group-4-book-a-meal-api.onrender.com/api/v1/users/1/user_menu_options'
+          `https://group-4-book-a-meal-api.onrender.com/api/v1/users/${user?.id}/user_menu_options`,
+          {
+            headers: {
+              Authorization: `Bearer ${user?.token}`,
+            },
+          }
         );
-        const data = await response.json();
-        dispatch(setMenuOptions(data));
+
+        if (response.ok) {
+          const data = await response.json();
+          dispatch(setMenuOptions(data));
+        } else {
+          // Handle unauthorized or other error responses
+          console.log('Error fetching menu options:', response.status);
+        }
       } catch (error) {
         console.log('Error fetching menu options:', error);
       }
     };
 
-    fetchData();
-  }, [dispatch]);
+    if (user) {
+      fetchData();
+    }
+  }, [dispatch, user]);
 
   return (
     <div>
-    <h2>Menu Options</h2>
-    {menuOptions.map((menuOption) => (
-      <div key={menuOption.id}>
-        <h3>{menuOption.name}</h3>
-        <p>{menuOption.description}</p>
-        <p>Caterer: {menuOption.caterer.business_name}</p>
-        <ul>
-          {menuOption.menu_options.map((option) => (
-            <li key={option.id}>
-              {option.name} - ${option.price}
-            </li>
-          ))}
-        </ul>
-      </div>
-    ))}
-  </div>
+      <h2>Menu Options</h2>
+      {menuOptions.map((menuOption) => (
+        <div key={menuOption.id}>
+          <h3>{menuOption.name}</h3>
+          <p>{menuOption.description}</p>
+          <p>Caterer: {menuOption.caterer.business_name}</p>
+          <ul>
+            {menuOption.menu_options.map((option) => (
+              <li key={option.id}>
+                {option.name} - ${option.price}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,55 +1,50 @@
-import React from "react";
-import { useAuth } from "./auth";
-import { useNavigate, useLocation } from "react-router-dom";
-import Menu from "./Menu";
+import React from 'react';
+import { useAuth } from './auth';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Menu from './Menu';
 
 const UserProfile = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
-  const redirectPath = location.state?.path || "/login";
+  const redirectPath = location.state?.path || '/login';
 
-  console.log("User:", user);
+  console.log('User:', user);
 
   function handleLogoutClick() {
-    fetch("https://crave-masters-front-end.onrender.com/api/v1/logout", { method: "DELETE" })
-      .then((res) => {
-        if (res.ok) {
-          logout();
-          navigate(redirectPath, { replace: true });
-        }
-      });
+    fetch('https://crave-masters-front-end.onrender.com/api/v1/logout', { method: 'DELETE' }).then((res) => {
+      if (res.ok) {
+        logout();
+        navigate(redirectPath, { replace: true });
+      }
+    });
   }
 
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-    {user ? (
-      <>
-        <h2 style={{ margin: "30px 30px 30px 250px", padding: "30px" }}>
-          Welcome, {user.username}!
-        </h2>
-        <button
-          onClick={handleLogoutClick}
-          style={{
-            display: "inline-block",
-            marginRight: "30px",
-            backgroundColor: "#34BB78",
-            color: "white",
-            padding: "10px 20px",
-            borderRadius: "5px",
-          }}
-        >
-          Logout
-        </button>
-        <Menu />
-      </>
-    ) : (
-      <h2 style={{ margin: "30px 30px 30px 250px", padding: "30px" }}>
-        Welcome, Guest. Please Log in!
-      </h2>
-    )}
-    {/* Additional content for the user profile */}
-  </div>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      {user ? (
+        <>
+          <h2 style={{ margin: '30px 30px 30px 250px', padding: '30px' }}>Welcome, {user.username}!</h2>
+          <button
+            onClick={handleLogoutClick}
+            style={{
+              display: 'inline-block',
+              marginRight: '30px',
+              backgroundColor: '#34BB78',
+              color: 'white',
+              padding: '10px 20px',
+              borderRadius: '5px',
+            }}
+          >
+            Logout
+          </button>
+          <Menu user={user} /> {/* Pass user object as a prop */}
+        </>
+      ) : (
+        <h2 style={{ margin: '30px 30px 30px 250px', padding: '30px' }}>Welcome, Guest. Please Log in!</h2>
+      )}
+      {/* Additional content for the user profile */}
+    </div>
   );
 };
 
